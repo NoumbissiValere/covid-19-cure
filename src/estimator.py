7ae8f009d6bba9b6839f560ff5d56b35a010280b
Noumbissi valere Gille Geovan <noumbissivalere@gmail.com>
2020-04-09 08:06:31 +0100

    solve challenge 1 of the assessement

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,43 @@
 def estimator(data):
-  return data
+    currentlyInfected = covid19ImpactEstimator(data['reportedCases'])
+    severeCurrentlyInfected = covid19ImpactEstimator(data['reportedCases'],
+                                                     severe=True)
+    infectionsByRequestedTime = getInfectionsByRequestedTime(currentlyInfected,
+                                                             data['periodType'],
+                                                             data['timeToElapse'])
+    severeInfectionsByRequestedTime = getInfectionsByRequestedTime(severeCurrentlyInfected,
+                                                                   data['periodType'],
+                                                                   data['timeToElapse'])
+    output = {
+        'data': data,
+        'impact': {
+            'currentlyInfected': currentlyInfected,
+            'infectionsByRequestedTime': infectionsByRequestedTime
+        },
+        'severeImpact': {
+            'currentlyInfected': severeCurrentlyInfected,
+            'infectionsByRequestedTime': severeInfectionsByRequestedTime
+        }
+    }
+    return output
+
+
+def covid19ImpactEstimator(reportedCases, severe=False):
+    if severe:
+        return reportedCases * 50
+    return reportedCases * 10
+
+
+def getDays(periodType, value):
+    if periodType == 'days':
+        return value
+    elif periodType == 'weeks':
+        return value * 7
+    else:
+        return value * 30
+
+
+def getInfectionsByRequestedTime(currentlyInfected, periodType, timeToElapse):
+    numDays = getDays(periodType, timeToElapse)
+    factor = numDays // 3
+    return currentlyInfected * pow(2, factor)
